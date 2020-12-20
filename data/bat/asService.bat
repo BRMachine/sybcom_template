9d6e22d57968ad4e09c66dd5882452bd7f964ec2
Rustem Arslanov <sobakasobaka6@gmail.com>
2020-12-20 17:11:14 +0700

    Добавлен скрипт для регистрации проекта как службы

---
+++ data/bat/asService.bat
@@ -0,0 +1,37 @@
+echo off
+echo "***********************************************"
+echo "***********Введи имя папки с проектом**********"
+echo "***********************************************"
+set /p PROJ="ProjName: "
+
+set /p "ch=путь до проекта "C:/WinccOA_Proj/%PROJ%/"? [y] " 
+IF "%ch%" == "Y" ( goto confirm )
+IF "%ch%" == "y" ( goto confirm )
+IF "%ch%" == "Д" ( goto confirm )
+IF "%ch%" == "д" ( goto confirm )
+IF "%ch%" == ""  ( goto confirm )
+goto end
+
+:confirm
+echo "Регистрация проекта в качестве службы запущена"
+WCCILpmon -install -set C:/WinCCOA_Proj/%PROJ%/config/config 1 -name SSPD-SRV-SVC
+echo "Регистрация завершена, остановите проект и запустите его как службу (SSPD-SRV-SVC)"
+echo "%userprofile%"
+xcopy "links\" "%userprofile%\desktop\" /Y 
+echo "Запуск проекта возможен только с ярлыков на рабочем столе"
+
+set /p "ch=Перезагрузить сервер сейчас? [y] " 
+IF "%ch%" == "Y" ( goto reboot )
+IF "%ch%" == "y" ( goto reboot )
+IF "%ch%" == "Д" ( goto reboot )
+IF "%ch%" == "д" ( goto reboot )
+IF "%ch%" == ""  ( goto reboot )
+goto end
+
+
+:reboot
+shutdown /r /t 000 /f
+goto end
+
+:end
+pause
